13e068d9af53ce1123db51201be423a40e43220d
piasekhgw <wojciech.piasecki@netguru.pl>
2015-02-21 15:33:57 +0100

    class calls handler - skeleton

--- lib/funky_api_client/response_handlers/class_call_handler.rb
+++ lib/funky_api_client/response_handlers/class_call_handler.rb
@@ -2,6 +2,15 @@ module FunkyApiClient
   module ResponseHandlers
     module ClassCallHandler
       def handle_class_call_response(response)
+        send("handle_class_call_#{response.success? : 'success' : 'error'}_response", response)
+      end
+
+      private
+
+      def handle_class_call_success_response(response)
+      end
+
+      def handle_class_call_error_response(response)
       end
     end
   end
